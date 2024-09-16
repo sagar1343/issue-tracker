c3e7c29ce74072255549d318ee6ae62c91a8a86e
Sagar Chakrawarti <sagarchakrawarti25@gmail.com>
2024-09-16 07:12:56 +0530

    Shows the issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,12 +1,32 @@
 import Link from "next/link";
 import React from "react";
+import prisma from "@/prisma/client";
 
-function IssuesPage() {
+async function IssuesPage() {
+  const issues = await prisma.issue.findMany();
   return (
-    <div>
+    <div className="space-y-8">
       <Link href="issues/new" className="btn btn-primary">
         New Issue
       </Link>
+      <table className="table">
+        <thead>
+          <th>Issue</th>
+          <th>Status</th>
+          <th className="hidden md:block">Created At</th>
+        </thead>
+        <tbody>
+          {issues.map((issues) => (
+            <tr>
+              <td>{issues.title}</td>
+              <td>{issues.status}</td>
+              <td className="hidden md:block">
+                {issues.createdAt.toLocaleString()}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
