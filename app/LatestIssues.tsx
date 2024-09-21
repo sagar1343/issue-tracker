7dbf0e7ff4c4905c90dca82af3673ccffd82898b
Sagar Chakrawarti <sagarchakrawarti25@gmail.com>
2024-09-22 03:38:38 +0530

    Build latest issues component

---
+++ app/LatestIssues.tsx
@@ -0,0 +1,44 @@
+import prisma from "@/prisma/client";
+import Link from "next/link";
+import IssuesBadge from "./components/IssuesBadge";
+import Image from "next/image";
+
+async function LatestIssues() {
+  const latestIssues = await prisma.issue.findMany({
+    orderBy: { createdAt: "desc" },
+    take: 5,
+    include: {
+      assignToUser: true,
+    },
+  });
+
+  return (
+    <div className="card card-compact card-bordered max-w-sm">
+      <div className="card-body">
+        <div className="card-title">Latest Issues</div>
+        {latestIssues.map((issue) => (
+          <div key={issue.id} className="flex items-center justify-between">
+            <div className="flex flex-col items-start gap-1">
+              <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+              <IssuesBadge status={issue.status} />
+            </div>
+            {issue.assignToUser && (
+              <div className="w-10">
+                <Image
+                  src={issue.assignToUser.image!}
+                  alt="asignee-image"
+                  height={25}
+                  width={25}
+                  referrerPolicy="no-referrer"
+                  className="rounded-full"
+                />
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default LatestIssues;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import LatestIssues from "./LatestIssues";
+
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <div>
+      <LatestIssues />
+    </div>
+  );
 }
